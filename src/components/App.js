0fcd726c76153b30d45c80bf8346a0802fafae82
kubusjestsuper <58520735+kubusjestsuper@users.noreply.github.com>
2021-02-23 11:05:48 +0100

    handlowanie sesji w klasie

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './Navigation';
@@ -11,23 +11,77 @@ import AccountPage from './Account';
 import AdminPage from './Admin';
 
 import * as ROUTES from '../constants/routes';
+import { withFirebase } from './Firebase/index';
 
-const App = () => (
-	<Router>
-		<div>
-			<Navigation />
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-			<hr />
+		this.state = {
+			authUser: null,
+		};
+	}
 
-			<Route exact path={ROUTES.LANDING} component={LandingPage} />
-			<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-			<Route path={ROUTES.SIGN_IN} component={SignInPage} />
-			<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-			<Route path={ROUTES.HOME} component={HomePage} />
-			<Route path={ROUTES.ACCOUNT} component={AccountPage} />
-			<Route path={ROUTES.ADMIN} component={AdminPage} />
-		</div>
-	</Router>
-);
+	componentDidMount() {
+		this.listener = this.props.firebase.auth.onAuthStateChanged((authUser) => {
+			authUser
+				? this.setState({ authUser })
+				: this.setState({ authUser: null });
+		});
+	}
 
-export default App;
+	componentWillUnmount() {
+		this.listener();
+	}
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<Navigation authUser={this.state.authUser} />
+					<hr />
+					<Route exact path={ROUTES.LANDING} component={LandingPage} />
+					<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+					<Route path={ROUTES.SIGN_IN} component={SignInPage} />{' '}
+					<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+					<Route path={ROUTES.HOME} component={HomePage} />
+					<Route path={ROUTES.ACCOUNT} component={AccountPage} />
+					<Route path={ROUTES.ADMIN} component={AdminPage} />
+				</div>
+			</Router>
+		);
+	}
+}
+
+// const App = (props) => {
+// 	const [authUser, setAuthUser] = useState(null);
+// 	const listener = useCallback(() => {
+// 		props.firebase.auth.onAuthStateChanged((authUser) => {
+// 			authUser ? setAuthUser({ authUser }) : setAuthUser({ authUser: null });
+// 		});
+// 	});
+
+// 	useEffect(() => {
+// 		listener();
+// 	}, [listener]);
+
+// 	return (
+// 		<Router>
+// 			<div>
+// 				<Navigation authUser={authUser} />
+
+// 				<hr />
+
+// 				<Route exact path={ROUTES.LANDING} component={LandingPage} />
+// 				<Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+// 				<Route path={ROUTES.SIGN_IN} component={SignInPage} />
+// 				<Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+// 				<Route path={ROUTES.HOME} component={HomePage} />
+// 				<Route path={ROUTES.ACCOUNT} component={AccountPage} />
+// 				<Route path={ROUTES.ADMIN} component={AdminPage} />
+// 			</div>
+// 		</Router>
+// 	);
+// };
+
+export default withFirebase(App);
