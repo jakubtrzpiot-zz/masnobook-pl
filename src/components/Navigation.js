9a4d2922e8b600b9af46b2187ea3be40d815572d
Bartosz Oliwiak <voozen3@gmail.com>
2021-02-23 17:53:46 +0100

    logo masnobook przenoszace na home/landing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navigation.scss";
 import { Link } from "react-router-dom";
+import logo from "../constants/masnobook-logo.png";
 
 import SignOutButton from "./SignOut";
 import * as ROUTES from "../constants/routes";
@@ -8,24 +9,34 @@ import * as ROUTES from "../constants/routes";
 const Navigation = ({ authUser }) => (
   <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
 );
-const NavigationAuth = () => (
-  <div className='signContain'>
-    <div className='signedIn'>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-      <br></br>
-      <Link to={ROUTES.HOME}>Home</Link>
-      <br></br>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-      <br></br>
-      <SignOutButton />
-    </div>
-  </div>
-);
+const NavigationAuth = () => {
+  return (
+    <>
+      <div className='signContain'>
+        <div className='logo'>
+          <Link to={ROUTES.HOME}>
+            <img src={logo} alt='masnobook' />
+          </Link>
+        </div>
+        <div className='signedIn'>
+          <Link to={ROUTES.LANDING}>Landing</Link>
+          <br></br>
+          <Link to={ROUTES.ACCOUNT}>Account</Link>
+          <br></br>
+          <SignOutButton />
+        </div>
+      </div>
+    </>
+  );
+};
 const NavigationNonAuth = () => (
   <div className='signContain'>
+    <div className='logo'>
+      <Link to={ROUTES.LANDING}>
+        <img src={logo} alt='masnobook' />
+      </Link>
+    </div>
     <div className='signedOut'>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-      <br></br>
       <Link to={ROUTES.SIGN_IN}>Sign In</Link>
     </div>
   </div>
