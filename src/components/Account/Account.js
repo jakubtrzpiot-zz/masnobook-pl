0e9b482301b8b96e6c3bfbca988b672230d934be
Bartosz Oliwiak <voozen3@gmail.com>
2021-02-24 09:05:49 +0100

    gitara

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './account.scss';
+import React from "react";
+import "./account.scss";
 
-import { AuthUserContext, withAuthorization } from '../Session';
+import { AuthUserContext, withAuthorization } from "../Session";
 
 const AccountPage = () => {
-	return (
-		<AuthUserContext.Consumer>
-			{(authUser) => (
-				<div>
-					<h1>Email: {authUser.email}</h1>
-					<h1>Uid: {authUser.uid}</h1>
-					<h1>Username: {authUser.username}</h1>
-				</div>
-			)}
-		</AuthUserContext.Consumer>
-	);
+  return (
+    <AuthUserContext.Consumer>
+      {(authUser) => (
+        <div className='Container'>
+          <h1>Email: {authUser.email}</h1>
+          <h1>Uid: {authUser.uid}</h1>
+          <h1>Username: {authUser.username}</h1>
+        </div>
+      )}
+    </AuthUserContext.Consumer>
+  );
 };
 const condition = (authUser) => !!authUser;
 export default withAuthorization(condition)(AccountPage);
